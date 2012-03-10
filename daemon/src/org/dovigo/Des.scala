368047fc9a1922dd51cf8b70051c47ac6e505edd
Hannes Moser <box@dovigo.com>
2012-03-10 19:46:25 +0100

    Adding Logging. Added Logging trait. Added slf4j. Added log4j.

--- daemon/src/org/dovigo/Des.scala
+++ daemon/src/org/dovigo/Des.scala
@@ -1,15 +1,19 @@
 package org.dovigo
+import org.dovigo.log.Logging
 
 /**
  * DES - Dovigo Encoding Server
  * 
  * @author dev
  * @version 0.1
+ * @since 0.1
  */
-object Des {
+object Des extends AnyRef with Logging {
 
   def main(args: Array[String]): Unit = {
-    println("DES - Dovigo Encoding Server")
+    
+    info("DES - Dovigo Encoding Server")
+    
   }
 
 }
---
+++ daemon/src/org/dovigo/log/Logging.scala
@@ -0,0 +1,20 @@
+package org.dovigo.log
+import org.slf4j.LoggerFactory
+
+/**
+ * Logging trait, wraps slf4j
+ * 
+ * @author Hannes Moser
+ * @version 0.1
+ * @since 0.1
+ */
+trait Logging {
+
+  var log = LoggerFactory.getLogger(getClass)
+  
+  def debug(msg: => String)	= if (log.isDebugEnabled) log.debug(msg)
+  def info(msg: => String)	= if (log.isDebugEnabled) log.info(msg)
+  def warn(msg: => String)	= if (log.isDebugEnabled) log.warn(msg)
+  def error(msg: => String) = if (log.isDebugEnabled) log.error(msg)
+  
+}
