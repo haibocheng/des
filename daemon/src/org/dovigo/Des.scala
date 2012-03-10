90bdd4499a78983fb52eb1fcd0ae59e651dbc773
Hannes Moser <box@dovigo.com>
2012-03-10 19:02:38 +0100

    Created directory and project structure.

---
+++ daemon/src/org/dovigo/Des.scala
@@ -0,0 +1,15 @@
+package org.dovigo
+
+/**
+ * DES - Dovigo Encoding Server
+ * 
+ * @author dev
+ *
+ */
+object Des {
+
+  def main(args: Array[String]): Unit = {
+    println("DES - Dovigo Encoding Server")
+  }
+
+}
